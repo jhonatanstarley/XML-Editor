bc8d212cf53ab51ca200381acf6d5d612df1865a
jhonatanstarley <jhonatanstarley@hotmail.com>
2024-07-16 02:40:21 -0300

    Update server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(fileUpload());
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 // Rota para renderizar o index.ejs
 app.get('/', (req, res) => {
